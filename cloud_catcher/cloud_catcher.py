04a8cc62b1f72547bb799966b3e614460bb31e37
Roman <romanranniew@gmail.com>
2022-03-03 13:18:21 +0100

    Refactor code

--- cloud_catcher/cloud_catcher.py
+++ cloud_catcher/cloud_catcher.py
@@ -1,14 +1,16 @@
-# ------------ STEP 2: start downloading! skip to catch function on the bottom
+# ------------ STEP 2: start downloading! skip to catch function
 import os
+import re
 from goes2go.data import goes_nearesttime
 import matplotlib.pyplot as plt
-from cloud_catcher.lib import ensure_dir
+from cloud_catcher.lib import ensure_dir, debug, mask_data, get_uv, setup_graph
 import gc
 from datetime import datetime
 import numpy
 import cartopy.crs as ccrs
 from toolbox.cartopy_tools import pc
-from scipy import interpolate
+
+profiling = True
 
 
 class CloudCatcher:
@@ -21,92 +23,31 @@ class CloudCatcher:
         self.cmap = cmap
         ensure_dir(dir)
 
-    def __save_image(self,  target_path, photo, wind):
-        print('Processing ', target_path)
-        print(*wind)
-        print(wind.wind_vectors_in_atmospheric_layer[0:2000:100])
-        print(wind.mean_cloud_top_pressure_in_atmospheric_layer)
-        # - Projections - https: // scitools.org.uk/cartopy/docs/latest/reference/projections.html
-        target_projection = ccrs.EquidistantConic(
-            central_longitude=-32.46, central_latitude=46.54)
-        imshow_args = {
-            **photo.rgb.imshow_kwargs,
-            'cmap': self.cmap,
-            'transform': photo.rgb.crs,
-            'interpolation': 'kaiser',
-            # - Interpolation methods - https://matplotlib.org/stable/gallery/images_contours_and_fields/interpolation_methods.html
-        }
-        max_coords = {
-            'north': 61,
-            'south': 4,
-            'west': -102,
-            'east': 0
-        }
-        band_str = 'CMI_C%02d' % self.band
-        photo_crop_bounds = [-120, 0, 0, 80]  # lon E, lon W, lat S,lat N
+    def catch(self, date):
+        # download .n and get the xarray data file
 
-        def get_time():
-            return datetime.now().strftime("%H:%M:%S")
+        photo = self.__get_photo_from_date(date)
+        wind = self.__get_wind_data_from_date(date)
 
-        # -- vars and transforms
-        linspace_steps = 10
-        print('masking', get_time())
-        mask = (wind.wind_speed.lat > max_coords['south']) & (wind.wind_speed.lat < max_coords['north']) & (
-            wind.wind_speed.lon > max_coords['west']) & (wind.wind_speed.lon < max_coords['east'])
-        wind_speed = wind.maximum_wind_speed.where(mask, drop=True)
-        # wind_speed = wind.wind_speed.where(
-        #     ~numpy.isnan(wind.wind_speed), drop=True)
-        wind_direction = wind.wind_direction.where(mask, drop=True)
-        # wind_direction = wind.wind_speed.where(
-        #     ~numpy.isnan(wind.wind_speed), drop=True)
-        x = wind.lon.where(mask, drop=True)
-        # x = wind.wind_speed.where(~numpy.isnan(wind.wind_speed), drop=True)
-        y = wind.lat.where(mask, drop=True)
-        # y = wind.wind_speed.where(~numpy.isnan(wind.wind_speed), drop=True)
-        # -- set up graph
-        plt.style.use('dark_background')
-        fig = plt.figure(figsize=(10, 10), edgecolor='black')
-        ax = fig.add_subplot(projection=target_projection,
-                             snap=True, frame_on=False)
-        ax.set_extent(photo_crop_bounds)
+        # - something went wrong downloading the data
+        if not photo or not wind:
+            return (False, date)
 
-        print('making grid...', get_time())
-        # - Wind-UV Components - http://colaweb.gmu.edu/dev/clim301/lectures/wind/wind-uv
-        u = -wind_speed * \
-            numpy.sin(numpy.radians(wind_direction))
-        v = -wind_speed * \
-            numpy.cos(numpy.radians(wind_direction))
-        SPD = numpy.sqrt(u**2+v**2)
-        SPD = numpy.nan_to_num(SPD)
+        photo_time = numpy.array2string(
+            photo['time_coverage_end']).replace('\'', '')
+        photo_name = datetime.strptime(
+            photo_time,
+            '%Y-%m-%dT%H:%M:%S.%fZ').strftime("%Y%m%dT%H%M%S")
+        target_path = os.path.join(
+            self.dir, f"{photo_name}.png").replace("\\", "/")
 
-        # levels = MaxNLocator(nbins=15).tick_values(
-        #     wind.wind_speed.min(), wind.wind_speed.max())
-        # -- draw wind
-        # speed_norm = plt.Normalize(0.0, SPD.max())
-
-        print('u')
-        # norm=speed_norm
-        # stream_lw = 2*SPD / SPD.max()
-        # linewidth = stream_lw m
-        print('Plotting...', get_time())
-        plt.quiver(wind_speed.lon, wind_speed.lat,
-                   u, v, SPD, pivot='mid', transform=pc, cmap='jet')
-        # **cm_wind().cmap_kwargs,
-
-        # -- draw photo
-        print('drawing photo', get_time())
-        ax.imshow(photo[band_str], **imshow_args)
-        ax.coastlines(linewidth=0.3, color="#00FF66", alpha=1)
-
-        # ---
-        print('Saving image: ', target_path)
-        # plt.show()
-        plt.savefig(target_path, dpi=self.dpi, bbox_inches='tight',
-                    facecolor='black', edgecolor='black')
-        # -- this is garbage
-        plt.close('all')
-        gc.collect()
-        del photo
+        if os.path.exists(target_path):
+            return ('photo exists', photo_name)
+        if photo:
+            # ------------  STEP 3: save the image! line 22
+            self.__save_image(target_path, photo, wind)
+            return (True, photo_name)
+        return (False, photo_name)
 
     def __get_photo_from_date(self, date):
         try:
@@ -133,23 +74,64 @@ class CloudCatcher:
             data = False
         return data
 
-    def catch(self, date):
-        # download .n and get the xarray data file
-        photo = self.__get_photo_from_date(date)
-        wind = self.__get_wind_data_from_date(date)
-        if not photo or not wind:
-            return (False, date)
-        photo_time = numpy.array2string(
-            photo['time_coverage_end']).replace('\'', '')
-        photo_name = datetime.strptime(
-            photo_time,
-            '%Y-%m-%dT%H:%M:%S.%fZ').strftime("%Y%m%dT%H%M%S")
-        target_path = os.path.join(
-            self.dir, f"{photo_name}.png").replace("\\", "/")
-        if os.path.exists(target_path):
-            return ('photo exists', photo_name)
-        if photo:
-            # ------------  STEP 3: save the image! line 22
-            self.__save_image(target_path, photo, wind)
-            return (True, photo_name)
-        return (False, photo_name)
+    def __save_image(self,  target_path, photo, wind):
+        print('Processing ', target_path)
+        # - Projections - https: // scitools.org.uk/cartopy/docs/latest/reference/projections.html
+        target_projection = ccrs.EquidistantConic(
+            central_longitude=-32.46, central_latitude=46.54)
+        imshow_args = {
+            **photo.rgb.imshow_kwargs,
+            'cmap': self.cmap,
+            'transform': photo.rgb.crs,
+            'interpolation': 'kaiser',
+            # - Interpolation methods - https://matplotlib.org/stable/gallery/images_contours_and_fields/interpolation_methods.html
+        }
+        max_coords = {
+            'north': 61,
+            'south': 4,
+            'west': -102,
+            'east': 0
+        }
+        band_str = 'CMI_C%02d' % self.band
+        photo_crop_bounds = [-120, 0, 0, 80]  # lon E, lon W, lat S,lat N
+
+        # for possibly future use
+        # linspace_steps = 10
+        # from cloud_catcher/lib import create_grid
+        # create_grid()
+
+        def get_time():
+            return datetime.now().strftime("%H:%M:%S")
+
+        ax = setup_graph(target_projection, photo_crop_bounds)
+
+        x, y, wind_speed, wind_direction = mask_data(
+            wind.wind_speed.lat, wind.wind_speed.lon, wind.wind_speed, wind.wind_direction, max_coords)
+        u, v = get_uv(wind_speed, wind_direction)
+
+        wind_speed_data = numpy.sqrt(u**2+v**2)
+        wind_speed_data = numpy.nan_to_num(wind_speed_data)
+
+        # -- draw wind
+        # stream_lw = 2*SPD / SPD.max()
+        # linewidth = stream_lw m
+        if profiling:
+            print('Plotting...', get_time())
+        plt.quiver(x, y,
+                   u, v, wind_speed_data, pivot='mid', transform=pc, cmap='jet')
+
+        # -- draw photo
+        if profiling:
+            print('drawing photo', get_time())
+        ax.imshow(photo[band_str], **imshow_args)
+        ax.coastlines(linewidth=0.3, color="#00FF66", alpha=1)
+
+        # ---
+        print('Saving image: ', target_path)
+        # plt.show()
+        plt.savefig(target_path, dpi=self.dpi, bbox_inches='tight',
+                    facecolor='black', edgecolor='black')
+        # -- this is garbage
+        plt.close('all')
+        gc.collect()
+        del photo
--- cloud_catcher/lib.py
+++ cloud_catcher/lib.py
@@ -1,6 +1,65 @@
-import os
-
-
 def ensure_dir(dir):
+    import os
     if not os.path.exists(dir):
         os.makedirs(dir)
+
+
+def create_grid(x, y, steps, data=False, u=False, v=False, ):
+    from scipy import interpolate
+    import numpy
+
+    xi = numpy.linspace(x.min().item(),
+                        x.max().item(), steps)
+    yi = numpy.linspace(y.min().item(),
+                        y.max().item(), steps)
+    X, Y = numpy.meshgrid(xi, yi)
+    if u:
+        u = interpolate.griddata((x, y), u, (X, Y), method='cubic')
+        u = numpy.nan_to_num(u)
+    if v:
+        v = interpolate.griddata((x, y), v, (X, Y), method='cubic')
+        v = numpy.nan_to_num(v)
+
+    if data:
+        data = interpolate.griddata(
+            (x, y), data, (X, Y), method='cubic')
+        data = numpy.nan_to_num(data)
+    return (X, Y, steps, data, u, v)
+
+
+def mask_data(x, y, ws, wd, max_coords):
+    # -- transform function and graph functions
+    mask = (x > max_coords['south']) & (x < max_coords['north']) & (
+        y > max_coords['west']) & (y < max_coords['east'])
+    ws = ws.where(mask, drop=True)
+    wd = wd.where(mask, drop=True)
+    x = x.where(mask, drop=True)
+    y = y.where(mask, drop=True)
+    return x, y, ws, wd
+
+
+def setup_graph(projection, extent):
+    import matplotlib.pyplot as plt
+    # -- set up graph
+    plt.style.use('dark_background')
+    fig = plt.figure(figsize=(10, 10), edgecolor='black')
+    print('seting up', projection)
+    ax = fig.add_subplot(projection=projection, extent=extent,
+                         snap=True, frame_on=False)
+    return ax
+
+
+def get_uv(wind_speed, wind_direction):
+    import numpy
+    # - Wind-UV Components - http://colaweb.gmu.edu/dev/clim301/lectures/wind/wind-uv
+    u = -wind_speed * \
+        numpy.sin(numpy.radians(wind_direction))
+    v = -wind_speed * \
+        numpy.cos(numpy.radians(wind_direction))
+    return u, v
+
+
+def debug(attrs):
+    print('DEBUG')
+    print('\n \033[92m--------------\033[0m \n'.join("%s: %s" %
+          item for item in attrs.items()))
